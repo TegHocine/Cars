13ca46d8c6ac5e3345caaacff0bc4a7e8bb50fa2
TegHocine <arcou98@hotmail.com>
2024-11-17 12:42:18 +0100

    init spring boot web app

---
+++ app/src/main/java/com/vineta/app/VinetaApplication.java
@@ -0,0 +1,13 @@
+package com.vineta.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinetaApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/vineta/app/VinetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vineta.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
